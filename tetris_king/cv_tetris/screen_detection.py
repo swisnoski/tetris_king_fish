d696bffcec21fa123d547c7f8e41ed59411943f7
yanashley <ashley.of.yang@gmail.com>
2025-11-22 00:04:33 -0500

    add first pass progress on screen detection
    up to screen edge detection stage, started playing around with color masking
    but currently not working yet

--- tetris_king/cv_tetris/screen_detection.py
+++ tetris_king/cv_tetris/screen_detection.py
@@ -1,6 +1,60 @@
-import cv2
+import cv2 as cv 
 
-# CV pipeline for detecting screen and gameplay grid area
+# ----------- Detecting TV screen -----------
+def detect_TV():
+    """
+    Detect TV Screen from initial Tetris camera image, and crop image
+    to just the screen contents.
+    """
+    # load in image
+    img = cv.imread("./assets/start_tetris.jpg")
+
+    # resize image so not massive
+    original_height, original_width = img.shape[:2]
+    new_width = 750
+    aspect_ratio = new_width / original_width # get new aspect ratio
+    new_height = int(original_height * aspect_ratio)
+    img = cv.resize(img, (new_width, new_height))
+
+    # grayscale 
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    show_close("Grayscaled", gray)
+
+    # blur image
+    blur = cv.bilateralFilter(gray, 9, 75, 75)
+    show_close("Blurred", blur)
+
+    # a color mask? since the black is so prominent 
+    # ycrcb = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
+    # skin_mask = cv.inRange(ycrcb, (0,133,77), (255,173,127))
+    # blur[skin_mask > 0] = 0
+    # show_close("Color Mask", )
+
+    # detect edges
+    edges = cv.Canny(blur, 100, 200)
+    show_close("Edges Detected", edges)
+
+    # morphological closing
+
+    # contour detection
+
+# ------- Detecting gameplay area -----------
 
 
-# CV for initalizing and masking the grid
+# ------ Initalizing and masking the grid --------
+def detect_grid():
+    pass
+
+def show_close(caption, img):
+    """
+    Helper function to simplify repeated showing and closing of cv windows
+
+    caption: a string for the img caption
+    img: CV image to show
+    """
+    cv.imshow(caption, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    detect_TV()
