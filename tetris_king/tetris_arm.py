05c6e561295b2d05e2fa5b86942f3315955924be
Bill Le <choangbill.le@gmail.com>
2025-12-08 15:18:00 -0500

    send commands through two different mc instances

--- tetris_king/tetris_arm.py
+++ tetris_king/tetris_arm.py
@@ -116,7 +116,7 @@ class TetrisArm(Node):
         Thread to move arm
         """
         self.mc.sync_send_angles(self.action["rotate"], 100, timeout=0.5)
-        self.mc.sync_send_angles(self.action["home"], 100, timeout=0.5)
+        self.mc2.sync_send_angles(self.action["home"], 100, timeout=0.5)
 
     def status_thread(self):
         """
