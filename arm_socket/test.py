a700cc2e6dff30f21c120bc0ea5f7693f88b27a3
Bill Le <choangbill.le@gmail.com>
2025-12-15 14:30:24 -0500

    test on robot on the fly

--- arm_socket/test.py
+++ arm_socket/test.py
@@ -4,29 +4,25 @@ import numpy as np
 import time
 from pymycobot import MyCobot280
 
+mc = MyCobot280("/dev/ttyAMA0", baudrate=1000000)
+
 # POS 1
 
 print("Starting IK")
-
-success_count = 0
-count = 0
-
-total_time = 0
 
 # for i in range(2):
 
 start_time = time.perf_counter()
 
-UP = [0.21, 0.008, 0.045]
-DOWN = [0.19, 0, 0.045]
-LEFT = [0.2, -0.013, 0.045]
-RIGHT = [0.2, 0.01, 0.045]
 HOME = [0.2, 0, 0.06]
 
-pos_list = [HOME, UP, DOWN, LEFT, RIGHT]
+while True:
 
-for pos in pos_list:
-    desired_ee = pos
+    x = input("x: ")
+    y = input("y: ")
+    z = input("z: ")
+
+    desired_ee = [float(x), float(y), float(z)]
     soln, err = inverse_kinematics(
         util.deg2rad([0, 0, 0, 0, 0, 0]), desired_ee, tol=0.001
     )
@@ -36,24 +32,18 @@ for pos in pos_list:
     soln = util.rad2deg(soln)
     soln[5] = 0
 
-    end_time = time.perf_counter()
-
     print(f"Solution: {soln}")
     print(f"Error: {err}")
 
-    elapsed_time = end_time - start_time
-    total_time = total_time + elapsed_time
-
-    if err < 0.01:
-        success_count = success_count + 1
-    count = count + 1
-
-    avg_time = total_time / count
-    success_rate = success_count / count
-
-    print(f"Success rate: {success_rate}")
-    print(f"Average time: {avg_time}")
-
-    print()
-    print()
-    print()
+    mc.send_angles(soln, 30)
+    mc.send_angles(
+        [
+            19.38766022032492,
+            -53.72804223828324,
+            -110.45016002179023,
+            74.17824998575445,
+            -2.1409152024860533e-05,
+            0,
+        ],
+        30,
+    )
