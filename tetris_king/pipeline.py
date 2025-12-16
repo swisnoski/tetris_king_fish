b4299b68c5997b40208a16e893b6e91901063e16
Oscar Bao <oscarbaoxianhao@gmail.com>
2025-12-16 01:30:24 -0500

    Try to fix cv update issue

--- tetris_king/pipeline.py
+++ tetris_king/pipeline.py
@@ -26,6 +26,7 @@ def loop():
     my_tetris = Tetris_PIPE()
     cap = initialize_video_capture()
     grid_pts = initialize_grid(cap)
+    last_list = []
     while True:
         current_piece_list = []
         select_piece_and_game = False
@@ -36,7 +37,7 @@ def loop():
             if current_piece is not None:
                 current_piece_list.append(current_piece)
                 game_state_current += game_state
-            if len(current_piece_list) == 3: 
+            if len(current_piece_list) == 3:
                 current_piece = current_piece_list[-1]
                 game_state_current[game_state_current < 2] = 0
                 game_state_current[game_state_current > 2] = 2
@@ -48,6 +49,10 @@ def loop():
                 break
 
         print(f"{current_piece}")
+        if last_list == current_piece_list:
+            continue
+        else:
+            last_list = current_piece_list
         try:
             np.testing.assert_array_equal(my_tetris.board[2:-1, 1:-1], game_state)
         except:
@@ -60,15 +65,14 @@ def loop():
         print(my_tetris.current_piece.type)
         print(my_tetris.board[2:-1, 1:-1])
         r, t = find_best_move(my_tetris.board, my_tetris.current_piece.type)
-        my_tetris.execute_moves(r, t) #update the board, no need to display
+        my_tetris.execute_moves(r, t)  # update the board, no need to display
         print(f"Rotation: {r}, Translations: {t}")
 
         message = str([r, t])
         client_socket.sendall(message.encode())
 
-        data = client_socket.recv(1024)
-        print(f"Server replies: {data.decode()}")
-        
+        # data = client_socket.recv(1024)
+        # print(f"Server replies: {data.decode()}")
 
 
 def main(args=None):
