830b59d31fefb9ad0b6366e1cf7acfaaaf6865dd
Oscar Bao <oscarbaoxianhao@gmail.com>
2025-12-15 22:08:30 -0500

    Random rt

--- tetris_king/pipeline.py
+++ tetris_king/pipeline.py
@@ -6,6 +6,7 @@ from threading import Thread
 from tetris_sim.tetris_pipeline import Tetris_PIPE
 from tetris_sim.tetris_max import find_best_move
 from cv_tetris.cv_pipeline import initialize_video_capture, initialize_grid, get_cv_info
+import random
 
 
 def loop():
@@ -16,28 +17,30 @@ def loop():
     client_socket.connect((SERVER_IP, SERVER_PORT))
     print("Connected to server.")
 
-    # Tetris pipeline
-    my_tetris = Tetris_PIPE()
-    cap = initialize_video_capture()
-    grid_pts = initialize_grid(cap)
+    # # Tetris pipeline
+    # my_tetris = Tetris_PIPE()
+    # cap = initialize_video_capture()
+    # grid_pts = initialize_grid(cap)
     while True:
-        current_piece = None
-        while current_piece is None:
-            game_state, current_piece = get_cv_info(cap, grid_pts)
-            if cv.waitKey(5) == ord("q"):
-                break
-        print(f"{current_piece}")
-        try:
-            np.testing.assert_array_equal(my_tetris.board[2:-1, 1:-1], game_state)
-        except:
-            # print("Discrepancy between CV detected board and internal board!")
-            # print(f"CV detected board:\n{game_state}")
-            # print(f"Internal board:\n{my_tetris.board[2:-1,1:-1]}")
-            my_tetris.board[2:-1, 1:-1] = game_state
+        #     current_piece = None
+        #     while current_piece is None:
+        #         game_state, current_piece = get_cv_info(cap, grid_pts)
+        #         if cv.waitKey(5) == ord("q"):
+        #             break
+        #     print(f"{current_piece}")
+        #     try:
+        #         np.testing.assert_array_equal(my_tetris.board[2:-1, 1:-1], game_state)
+        #     except:
+        #         # print("Discrepancy between CV detected board and internal board!")
+        #         # print(f"CV detected board:\n{game_state}")
+        #         # print(f"Internal board:\n{my_tetris.board[2:-1,1:-1]}")
+        #         my_tetris.board[2:-1, 1:-1] = game_state
 
-        my_tetris.update_piece(current_piece)
-        r, t = find_best_move(my_tetris.board, my_tetris.current_piece.type)
-        my_tetris.execute_moves(r, t)  # update the board, no need to display
+        #     my_tetris.update_piece(current_piece)
+        # r, t = find_best_move(my_tetris.board, my_tetris.current_piece.type)
+        # my_tetris.execute_moves(r, t)  # update the board, no need to display
+        r = random.randint(0, 3)
+        t = random.randint(-4, 4)
 
         message = str([r, t])
         client_socket.sendall(message.encode())
